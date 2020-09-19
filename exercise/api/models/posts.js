5df714fdbe19704248129f9b2946ff657b9d2d87
Ivan Davis <techbyivan@users.noreply.github.com>
2020-09-19 12:48:09 -0400

    Got api endpoints rendered
    Got api endpoints rendered

---
+++ exercise/api/models/posts.js
@@ -0,0 +1,37 @@
+const PATH = "./data.json"
+const fs = require("fs");
+
+class Post {
+	get() {
+		// get post
+		return this.readData();
+	}
+
+	getIndividualBlog(postId) {
+		// get one blog post
+		const posts = this.readData();
+		const foundPost = posts.find((post) => post.id == postId);
+		return foundPost;
+	}
+
+	add(newPost) {
+		// add new post
+		const currentPosts = this.readData();
+		currentPosts.unshift(newPost)
+		this.storeData(currentPosts)
+	}
+
+	readData() {
+		// reads blog api
+		let rawData = fs.readFileSync(PATH);
+		let posts = JSON.parse(rawData);
+		return posts;
+	}
+
+	storeData(rawData) {
+		let data = JSON.stringify(rawData);
+		fs.writeFileSync(PATH, data)
+	}
+}
+
+module.exports = Post;
--- exercise/app.js
+++ exercise/app.js
@@ -1,10 +1,21 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const Post = require("./api/models/posts");
+const postsData = new Post();
+
+app.get("/api/posts", (req, res) => {
+	res.status(200).send(postsData.get());
+});
+
+app.get("/api/posts/:post_id", (req, res) => {
+    const postId = req.params.post_id;
+    const foundPost = postsData.getIndividualBlog(postId)
+    if (foundPost) {
+        res.status(200).send(foundPost)
+    } else {
+        res .status(400).send("Not Found")
+    }
+});
 
 
-app.get("/", (req, res) => {
-    res.status(200).send("hello world")
-})
-
-app.listen(3000, () => console.log("listening on http://localhost:3000"))
-
+app.listen(3003, () => console.log("listening on http://localhost:3003"));
