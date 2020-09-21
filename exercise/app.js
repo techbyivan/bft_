508cbe77aa05ee35b2559c47421b2c41bb60727e
Ivan Davis <techbyivan@users.noreply.github.com>
2020-09-21 13:23:15 -0400

    updated api
    updated api

--- exercise/app.js
+++ exercise/app.js
@@ -2,6 +2,30 @@ const express = require("express");
 const app = express();
 const Post = require("./api/models/posts");
 const postsData = new Post();
+var multer = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+
+const getExt = (mimeType) => {
+    switch (mimeType) {
+        case "image/png":
+            return ".png";
+        case "image/jpeg":
+            return ".jpeg";
+    }
+}
+
+var upload = multer({storage: storage})
+
+
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -24,5 +48,17 @@ app.get("/api/posts/:post_id", (req, res) => {
     }
 });
 
+
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.file.path,
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newPost);
+    res.status(201).send(newPost);
+})
 
 app.listen(3000, () => console.log("listening on http://localhost:3000"));
